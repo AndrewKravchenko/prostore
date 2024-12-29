582666ffb6f8c4d9e462332440a8247244d87d43
Andrey <andreikrauchanka1994@gmail.com>
2024-12-29 14:59:11 +0300

    feat: add List sample products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
+import sampleData from '@/db/sample-data'
+import { ProductList } from '@/entities'
+
 const Home = () => {
-  return <>Prostore</>
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList title="Newest Arrivals" data={sampleData.products} />
+    </div>
+  )
 }
 
 export default Home
